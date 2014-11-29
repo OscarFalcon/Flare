e4b8cc3638c168bd9d5b80d103fdd9ce44ef39be
oscarfalcon <oscar.falcon@live.com>
2014-11-29 00:58:26 -0600

    made flare service class where all related services like get events will go

--- src/com/flare/servlets/MapServlet.java
+++ src/com/flare/servlets/MapServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
 
 import com.flare.database.MySQL;
+import com.flare.services.FlareService;
 /**
  * Servlet implementation class MapServlet
  */
@@ -55,73 +56,8 @@ public class MapServlet extends BaseServlet {
 			System.out.println("MapServlet:POST: username is null");
 			return;
 		}
-		
-		
-		String mysql_string = "SELECT event.id,event.title,event.description,event.locationLat,event.locationLong"
-							  + ",event.date,event.time,event.type,event.attending,event.checked_in,friends.friend_id from"
-							  + " event INNER JOIN friends ON friends.friend_id = event.user_id WHERE friends.user_id = ?";
-		
-		ArrayList<Object[]> results;
-		Object[] arguments = new Object[]{user_id};
-		int[] result_types = new int[]{MySQL.INTEGER,MySQL.STRING,MySQL.STRING,MySQL.BIG_DECIMAL,MySQL.BIG_DECIMAL,MySQL.STRING,
-									   MySQL.STRING,MySQL.STRING,MySQL.INTEGER,MySQL.INTEGER,MySQL.INTEGER};
-		
-
-		results = MySQL.executeQuery(mysql_string, arguments, result_types);
-		
-		if(results == null)
-		{
-			response.setStatus(503);	//503 Service Unavailable.
-			return;
-		}
-		
-		StringBuilder json = new StringBuilder("{ \"events\": [");
-		
-		int size = results.size();
-		for(int i = 0; i < size;i++)
-		{
-			Object tmp[]; 
-			String line = null;
-			tmp = results.get(i);
-			String eventTitle,eventDescription,eventDate,eventTime,eventType;
-			BigDecimal locationLat,locationLog;
-			int eventId,eventAttending,eventCheckedIn,friendId;
-			
-			
-			eventId = (int) tmp[0];
-			eventTitle = (String) tmp[1];
-			eventDescription = (String) tmp[2];
-			locationLat = (BigDecimal) tmp[3];
-			locationLog = (BigDecimal) tmp[4];
-			eventDate = (String) tmp[5];
-			eventTime = (String) tmp[6];
-			eventType = (String) tmp[7];
-			eventAttending = (int) tmp[8];
-			eventCheckedIn = (int) tmp[9];
-			friendId = (int) tmp[10];
-			 
-			line  = "{ \"eventId\":" + "\"" + eventId + "\"," +
-					"\"eventTitle\":" + "\""+ eventTitle + "\"," +
-					"\"eventDescription\":" + "\"" + eventDescription + "\"," +
-					"\"eventDate\":" + "\"" + eventDate + "\"," +
-					"\"eventTime\":" + "\"" + eventTime +"\"," +
-					"\"eventType\":" + "\"" + eventType + "\"," +
-					"\"locationLat\":" +"\""+ locationLat + "\"," +
-					"\"locationLog\":" +"\""+locationLog + "\"," + 
-					"\"attending:\":" + "\""+eventAttending + "\"," +
-					"\"checkedIn\":" + "\""+eventCheckedIn +"\"," +
-					"\"friendId\":" + "\""+friendId + "\"}";
-			if( (i + 1) != size)
-			{
-				line = line + ",";
-			}
-			
-			json.append(line);
-			
-		}
-		json.append("]}");
+		String json = FlareService.getEvents(Integer.parseInt(user_id));
 		System.out.println(json);
-		
 		writer.write(json.toString());
 		writer.close();
 		return;
