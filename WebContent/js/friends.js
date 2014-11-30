0051a41abaf7c48c29fea0e772388384aaa53e17
amartine <aj01_martinez@yahoo.com>
2014-11-29 22:41:00 -0600

    finished friendslist, still need to add button for "unfollow" and functionality for it.

---
+++ WebContent/js/friends.js
@@ -0,0 +1,33 @@
+
+function loadFriends(){
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function (){
+		if(xhr.status == 200){
+			console.log("response: " + xhr.responseText);
+			var response = JSON.parse(xhr.responseText);
+			
+			var htmlCode = "";
+			for (i=0; i<response.friends.length; i++){
+				console.log("Loop" + i);
+				htmlCode += ""+
+				'<div data-role="content">'+
+					'<div style="" data-controltype="image">'+
+						'<img class="round" style=" float: left; width: 75px; height: 75px" src="http://localhost:8080/Profile/'+ response.friends[i].friend_id+'.jpg">'+
+						'<h2>'+ response.friends[i].firstName+ ' ' + response.friends[i].lastName + '</h2></br>'+
+						response.friends[i].username + '</br>'+
+						response.friends[i].aboutMe +
+					'</div>'+
+				'</div>' +
+				'<hr style="height:3px; background-color:#ccc; border:0; margin-top:12px; margin-bottom:12px;">';
+			}
+			
+			document.getElementById("friendsList").innerHTML = htmlCode;
+			console.log("success!");
+		}
+		
+		
+	}
+	
+	xhr.open("POST", "http://localhost:8080/friends", false);
+	xhr.send();
+}
