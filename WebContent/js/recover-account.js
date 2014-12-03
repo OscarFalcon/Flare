9c5de557c117aa280963341c5639ab44bd80ce9a
Mike Price <m.price83@yahoo.com>
2014-12-03 15:29:39 -0600

    push recover page

--- WebContent/js/recover-account.js
+++ WebContent/js/recover-account.js
@@ -1,4 +1,41 @@
-/**
- * 
- */
+function recoverAccount()
+{
+	
+	var url = "http://localhost:8080/recoverAccount";
+	var method = "POST";
+	var postData;
+	var async = true;
+	
+	postData = "email=" + email;
+			   
+	var request = new XMLHttpRequest();
+	request.onload = function () {
+
+		var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+		var response = request.responseText; // Returned data, e.g., an HTML document.
+		console.log("status: " + status);
+		console.log("response: " + response);
+	   
+		if(status == 200 && response == "true")
+		{
+			document.getElementById('error').innerHTML="<font color=\"green\">Reset Account!</font>";
+			alert("Updated Account")
+			document.getElementById('error').innerHTML="<font color=\"green\">Redirecting to login page.. </font>";
+			sleep(1000);
+			window.location.replace("http://localhost:8080/recoverAccount");
+		   		  
+		}
+		else
+		{
+			document.getElementById('error').innerHTML="<font color=\"red\">Failed to enter correct e-mail address </font>";
+		}
+	   
+	}
+	request.open(method,url,async);
+	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	request.send(postData); 
+	return false;
+	
+}
+
 
