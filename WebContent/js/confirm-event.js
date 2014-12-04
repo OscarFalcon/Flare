f810562562989b40749c7ced389d7fbef36ed6e8
oscarfalcon <oscar.falcon@live.com>
2014-12-04 01:11:13 -0600

    merging

--- WebContent/js/confirm-event.js
+++ WebContent/js/confirm-event.js
@@ -1,6 +1,13 @@
-window.CodiqaControls && window.CodiqaControls.register('googlemaps', 'googlemapsjs3',{
-              ready: start
-});
+
+
+document.onload = function()
+{
+	console.log("start");
+	start();
+}
+
+
+var mainControl;
 
 var styles = [
               {
@@ -142,8 +149,18 @@ var styles = [
 
 
 
-function start(control)
+function start()
 {
+	document.getElementById("searchInput").addEventListener("keydown", updateMap);
+	
+	var control = new google.maps.Map(document.getElementById('googlemapsjs3'));
+
+	
+	
+	mainControl = control;
+	
+	
+	
 	console.log("confirm-event");
 	/** initialize control **/	
     control.options = {
@@ -185,5 +202,57 @@ function start(control)
 		alert('error: ' + msg);
 	}
 }
+
+function updateMap(event)
+{
+	if (event.keyCode != 13)
+	{
+		return;
+	}
+	
+	var service = new google.maps.places.PlacesService(mainControl.map);
+	console.log("created service");
+	
+	var currentPosition;
+	/** set center of map to current user location **/
+	navigator.geolocation.getCurrentPosition(success,error);
+	function success(position) {
+		currentPosition =  new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+	}
+	function error(){
+		alert("Oops! Something Went Wrong");
+	}
+	
+	var placeName = document.getElementById("searchInput").value;
+
+	 var request = {
+			    location: currentPosition,
+			    radius: '10000',
+			    name: placeName
+			  };
+		
+	 console.log("nearbySearch Call");
+	 service.textSearch(request, callback);
+	 
+
+	
+}
+
+function callback(results, status) {
+	console.log("call Back");
+	  if (status == google.maps.places.PlacesServiceStatus.OK) {
+		 console.log("service OK");
+	     for (var i = 0; i < results.length; i++) {
+	        var place = results[i];
+	        console.log("result: " + results[i]);
+	      //createMarker(results[i]);
+	     }
+	  }
+	  else
+		  console.log("service NOTOK");
+	}
+
+
+
 
 
